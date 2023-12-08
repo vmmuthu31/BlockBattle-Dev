adc2ebf4b4f07c77bfe2b49d12c11a756669a7ee
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-12-09 00:42:59 +0530

    updated the packages and the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import Navbar from "./Components/Navbar";
+import { Loader, PerformanceMonitor, SoftShadows } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
+import { Physics } from "@react-three/rapier";
+import { Suspense, useState } from "react";
+import { Experience } from "./components/Experience";
+import { Leaderboard } from "./Components/Leaderboard";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [downgradedPerformance, setDowngradedPerformance] = useState(false);
   return (
-    <div className="App">
-      <Navbar />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="root">
+      <Loader />
+      <Canvas
+        shadows
+        camera={{ position: [0, 30, 0], fov: 30, near: 2 }}
+        dpr={[1, 1.5]}
+      >
+        <color attach="background" args={["#242424"]} />
+        <SoftShadows size={42} />
+
+        <PerformanceMonitor
+          onDecline={(fps) => {
+            setDowngradedPerformance(true);
+          }}
+        />
+        <Suspense>
+          <Physics>
+            <Experience downgradedPerformance={downgradedPerformance} />
+          </Physics>
+        </Suspense>
+        {!downgradedPerformance && (
+          <EffectComposer disableNormalPass>
+            <Bloom luminanceThreshold={1} intensity={1.5} mipmapBlur />
+          </EffectComposer>
+        )}
+      </Canvas>
+      <Leaderboard />
     </div>
   );
 }
