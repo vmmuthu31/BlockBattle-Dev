29c75c428f24ee792de12a9fa0f57de23f092013
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-12-09 01:41:58 +0530

    updated the first batch of the contract integration function with our contract address

---
+++ src/config/BlockchainServices.jsx
@@ -0,0 +1,122 @@
+import Web3 from "web3";
+import ABI from "../../contracts/abi.json";
+import { ethers } from "ethers";
+
+const isBrowser = () => typeof window !== "undefined"; //The approach recommended by Next.js
+const { ethereum } = isBrowser();
+if (ethereum) {
+  isBrowser().web3 = new Web3(ethereum);
+  isBrowser().web3 = new Web3(isBrowser().web3.currentProvider);
+}
+const ContractADD = "0x7ED61251F9e8f088e00ceE73CC086dC3c123e2F5";
+
+export const registerusr = async ({ playeradd, playername }) => {
+  const provider =
+    window.ethereum != null
+      ? new ethers.providers.Web3Provider(window.ethereum)
+      : ethers.providers.getDefaultProvider();
+  const signer = provider.getSigner();
+  const Role = new ethers.Contract(ContractADD, ABI, signer);
+  const tokenId = await Role.registerPlayer(playeradd, playername);
+  return tokenId;
+};
+
+export const isusrregistered = async ({ playeradd }) => {
+  const provider =
+    window.ethereum != null
+      ? new ethers.providers.Web3Provider(window.ethereum)
+      : ethers.providers.getDefaultProvider();
+  const signer = provider.getSigner();
+  const Role = new ethers.Contract(ContractADD, ABI, signer);
+  const tokenId = await Role.isPlayerRegistered(playeradd);
+  return tokenId;
+};
+
+export const addgunassets = async ({
+  gunid,
+  name,
+  imghash,
+  price,
+  rateoffire,
+  gunrange,
+}) => {
+  const provider =
+    window.ethereum != null
+      ? new ethers.providers.Web3Provider(window.ethereum)
+      : ethers.providers.getDefaultProvider();
+  const signer = provider.getSigner();
+  const Role = new ethers.Contract(ContractADD, ABI, signer);
+  const tokenId = await Role.addGun(
+    gunid,
+    name,
+    imghash,
+    price,
+    rateoffire,
+    gunrange
+  );
+  return tokenId;
+};
+
+export const addcharassets = async ({
+  charid,
+  name,
+  imghash,
+  price,
+  strength,
+  armorpower,
+}) => {
+  const provider =
+    window.ethereum != null
+      ? new ethers.providers.Web3Provider(window.ethereum)
+      : ethers.providers.getDefaultProvider();
+  const signer = provider.getSigner();
+  const Role = new ethers.Contract(ContractADD, ABI, signer);
+  const tokenId = await Role.addCharacter(
+    charid,
+    name,
+    imghash,
+    price,
+    strength,
+    armorpower
+  );
+  return tokenId;
+};
+
+export const addvechileassets = async ({
+  vehicleId,
+  name,
+  imghash,
+  price,
+  range,
+  speed,
+}) => {
+  const provider =
+    window.ethereum != null
+      ? new ethers.providers.Web3Provider(window.ethereum)
+      : ethers.providers.getDefaultProvider();
+  const signer = provider.getSigner();
+  const Role = new ethers.Contract(ContractADD, ABI, signer);
+  const tokenId = await Role.addVehicle(
+    vehicleId,
+    name,
+    imghash,
+    price,
+    range,
+    speed
+  );
+  return tokenId;
+};
+
+export const buyasset = async ({ playeradd, assetid, assettype, price }) => {
+  const provider =
+    window.ethereum != null
+      ? new ethers.providers.Web3Provider(window.ethereum)
+      : ethers.providers.getDefaultProvider();
+  const signer = provider.getSigner();
+  const Role = new ethers.Contract(ContractADD, ABI, signer);
+  const tokenId = await Role.buyAsset(playeradd, assetid, assettype, {
+    value: price,
+  });
+  console.log(tokenId);
+  return tokenId;
+};
