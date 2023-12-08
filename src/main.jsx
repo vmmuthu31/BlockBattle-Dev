be70dec07e385340b21042faeae2db581339374f
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-12-09 00:31:49 +0530

    updated the navbar with rainbow wallet kit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../store/store";
 import { LightNodeProvider } from "@waku/react";
-import { LightNodeProvider } from "@waku/react";
 import { Protocols } from "@waku/sdk";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
