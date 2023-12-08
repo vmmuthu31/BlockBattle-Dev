045ab19b46f24ced10536975ecf66dce641d4d99
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-12-08 23:45:58 +0530

    updated the waku wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,23 @@ import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../store/store";
+import { LightNodeProvider } from "@waku/react";
+import { LightNodeProvider } from "@waku/react";
+import { Protocols } from "@waku/sdk";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <Provider store={store}>
-        <Routes>
-          <Route path="/" exact element={<App />} />
-        </Routes>
-      </Provider>
+      <LightNodeProvider
+        options={{ defaultBootstrap: true }}
+        protocols={[Protocols.Store, Protocols.Filter, Protocols.LightPush]}
+      >
+        <Provider store={store}>
+          <Routes>
+            <Route path="/" exact element={<App />} />
+          </Routes>
+        </Provider>
+      </LightNodeProvider>
     </Router>
   </React.StrictMode>
 );
