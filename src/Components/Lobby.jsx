1f5a69465fed389b1c717d318c4c1cbe80568a0c
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-12-10 06:09:37 +0530

    updated the map models

--- src/Components/Lobby.jsx
+++ src/Components/Lobby.jsx
@@ -6,7 +6,39 @@ import Link from "next/link";
 import { getPlayerData } from "../config/BlockchainServices";
 import { useAccount } from "wagmi";
 
+var map_count = 0;
 function Lobby() {
+  const map_path = [
+    "https://blogger.googleusercontent.com/img/a/AVvXsEiIDKYobYMAxdl5gAtBoE7B8P9G8iB0AYJUfiA0kR0NubthcLBo_LyYjsajGpA0jr6B1mCVB0lG5ZhMnhFYjNtbY5CiE6PJYmlXaAv5-TZ9GFJjnNZhLCulC76CPvjJfPmfIq3_5bvh0U7N7g784SznhnU5qS_uaRzeL2RsDlx39RboomQP1eg_MmahpNY",
+    "https://blogger.googleusercontent.com/img/a/AVvXsEgHxU-HB-lQ9ifrEy-ymcHR6aeTkwzBaOsIQ6SXinjXyVVmqCbtY44ZraIGYM86B6DT7vk3jDrQSbdJn61D6jZB3HX3aRSc7EIYnSStvJmZefxCOpcKRZVFqha7jg0dd4i-0qZN-87FqviZbUY3oODu3bvJZK9ytVKnLRYcgFpo9hz4JzK25BmQS5c9TMI",
+  ];
+
+  const [map_count, setMap_count] = useState(0);
+  var i = 0;
+  const [img_v, setImg] = useState(map_path[i]);
+  const image_set = (move) => {
+    console.log(move, i);
+    if (i > map_path.length - 1 || i < 0) {
+      i = 0;
+    } else {
+      if (move === 1) {
+        i++;
+      } else {
+        i--;
+      }
+    }
+    if (move === 1) {
+      setImg(map_path[i]);
+      localStorage.setItem("map", map_path[i]);
+    } else if (move === 2) {
+      localStorage.setItem("map", map_path[i]);
+      setImg(map_path[i]);
+    }
+    if (map_path[i] === undefined) {
+      setImg(map_path[0]);
+    }
+    console.log(localStorage.getItem("timed"), localStorage.getItem("map"));
+  };
   const gltf = useLoader(GLTFLoader, "./models/Character_Soldier.gltf");
   const { address, isConnected } = useAccount();
   const playerAddress = address;
@@ -20,6 +52,34 @@ function Lobby() {
     }
     getplayere();
   }, [address]);
+
+  const map_names = ["pochinki", "pochinki", "Israel"];
+  const updateData = async () => {
+    console.log({
+      map: localStorage.getItem("map"),
+      time: localStorage.getItem("timed"),
+    });
+    try {
+      const response = await fetch(
+        "https://block-shotter-api.vercel.app/map_info",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            map: localStorage.getItem("map"),
+            time: localStorage.getItem("timed"),
+          }),
+        }
+      );
+
+      const data = await response.json();
+      console.log("Updated data:", data);
+    } catch (error) {
+      console.error("Error updating data:", error.message);
+    }
+  };
 
   return (
     <>
@@ -94,45 +154,104 @@ function Lobby() {
               <OrbitControls target={[0, 1, 0]} />
             </Canvas>
           </div>
-          <div className="mt-56 ">
-            <div className="flex mapbox px-6 py-3 items-center space-x-5">
-              <img
-                src="https://blogger.googleusercontent.com/img/a/AVvXsEhJ85zCrUSpRV7cSOt5Y1VeibTq8106ipzp_Ow_LZxxFvl2BDdUTpR0N5LVWnfhcA8DjymoCzOOgAl_3P4kpI9QXB2MJBEm6DP1n6kbleCpf_8IY_uaucIZpKyAwZjNJd9XzG2GRbyyqMhX5FKrNeKg1UAj0WLoxEA8b9hKg-eXqJi7IralLJYl8fnj2Uk"
-                alt=""
-                className="h-8 w-auto"
-              />
-              <p>Select Map</p>
-            </div>
-            <div className="">
-              <img
-                src="https://blogger.googleusercontent.com/img/a/AVvXsEhLX30N2aeEQt2Iv9xNfAUagS2HOylDbudaDn_uyM3NvtggvfRsS1vtFPFNdsTGoXFrloA-dYaXtJDkztDAc-hxIzsHhI_XcZycIV0Az6fTR8jxGWl2lgd8PQPpLaoZ3KWgTubfv9-x2RfEyqM7mmBq3SoytIX0AH8jKNFgKEw7UeENmC4fERXuwnaA9JA"
-                className="h-40 w-auto"
-                alt=""
-              />
-              <div className="absolute   top-[525px]">
-                <p className="text-center  bg-black bg-opacity-40 py-2 px-[80px]">
-                  Bermuda
-                </p>
+          <div className="mt-48 ">
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignContent: "center",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <div className="flex mapbox px-6 py-3 w-72 items-center space-x-5">
+                <img
+                  src="https://blogger.googleusercontent.com/img/a/AVvXsEhJ85zCrUSpRV7cSOt5Y1VeibTq8106ipzp_Ow_LZxxFvl2BDdUTpR0N5LVWnfhcA8DjymoCzOOgAl_3P4kpI9QXB2MJBEm6DP1n6kbleCpf_8IY_uaucIZpKyAwZjNJd9XzG2GRbyyqMhX5FKrNeKg1UAj0WLoxEA8b9hKg-eXqJi7IralLJYl8fnj2Uk"
+                  alt=""
+                  className="h-8 w-auto"
+                />
+                <p>Select Map</p>
+              </div>
+              <div className="">
+                <img src={img_v} className="h-40  w-auto" alt="" />
+                <div
+                  className="bg-black"
+                  style={{
+                    display: "flex",
+                    background: "",
+                    flexDirection: "row-reverse",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    flexWrap: "nowrap",
+                  }}
+                >
+                  <button
+                    className="px-2  bg-[#2f2f2f]"
+                    onClick={() => {
+                      if (map_count < 2) {
+                        image_set(1);
+                        setMap_count(map_count + 1);
+                      }
+                    }}
+                  >
+                    {">"}
+                  </button>
+                  <p className="text-center   bg-opacity-40 py-2  w-[68px]">
+                    {map_names[map_count]}
+                  </p>
+                  <button
+                    className="px-2 bg-[#2f2f2f]"
+                    onClick={() => {
+                      if (map_count > 1) {
+                        image_set(2);
+                        setMap_count(map_count - 1);
+                      }
+                    }}
+                  >
+                    {"<"}
+                  </button>
+                </div>
               </div>
             </div>
-            <div className="flex justify-between mx-2 items-center mt-3">
-              <p className="px-3 text-lg hover:text-black hover:bg-[#9FC610]  py-1 border border-[#9FC610] rounded-xl text-[#9FC610]">
+
+            <div
+              className="flex justify-between mx-2 items-center mt-3"
+              onClick={() => {
+                localStorage.setItem("time", "60");
+              }}
+            >
+              <p
+                onClick={() => {
+                  localStorage.setItem("timed", "60");
+                }}
+                className="px-3 text-lg hover:text-black hover:bg-[#9FC610] py-1 border border-[#9FC610] rounded-xl text-[#9FC610]"
+              >
+                1 min
+              </p>
+              <p
+                onClick={() => {
+                  localStorage.setItem("timed", "300");
+                }}
+                className="px-3 text-lg hover:text-black hover:bg-[#9FC610] py-1 border border-[#9FC610] rounded-xl text-[#9FC610]"
+              >
                 5 min
               </p>
-              <p className="px-3 text-lg hover:text-black hover:bg-[#9FC610]  py-1 border border-[#9FC610] rounded-xl text-[#9FC610]">
+              <p
+                onClick={() => {
+                  localStorage.setItem("timed", "600");
+                }}
+                className="px-3 text-lg hover:text-black hover:bg-[#9FC610] py-1 border border-[#9FC610] rounded-xl text-[#9FC610]"
+              >
                 10 min
-              </p>
-              <p className="px-3 text-lg hover:text-black hover:bg-[#9FC610]  py-1 border border-[#9FC610] rounded-xl text-[#9FC610]">
-                {" "}
-                15 min
               </p>
             </div>
             <div className="flex justify-center">
-              <Link href="/game">
-                <button className="playbtm px-10 font-semibold  py-2 mt-5 text-xl text-black ">
-                  Play!!
-                </button>
-              </Link>
+              <a
+                className="playbtm px-10 font-semibold  py-2 mt-5 text-xl text-black "
+                href="/game"
+              >
+                Play!!
+              </a>
             </div>
           </div>
         </div>
